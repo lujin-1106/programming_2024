7302a22a832c0e6626ca200f10ac69161f7a7609
lujin-1106 <1815603@sookmyung.ac.kr>
2024-10-30 14:13:33 +0900

    9주차실습

---
+++ Week09/Project1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int flag = 1;
+	
+	while(flag!=0){
+		int y;
+		y = 3;
+		flag = 0;
+		
+		y=4;
+	}
+	
+	//y=4; //변수 y는 while {} 내에서만 존재하는 지역 변수이므로 while 밖에서 존재하지 않는다.  
+	
+	return 0;
+}
---
+++ Week09/sub.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int all_files;
+void sub(void){
+	all_files=10;
+}
+
+/* extern int sum;
+
+int sum1(void){
+	sum = sum +1;
+}
+*/
